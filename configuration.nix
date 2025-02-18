e3d83ab12b6562a2b526d0325a3a649695807042
Fabien RIBES <fabien.ribes@eove.fr>
2025-02-18 18:43:17 +0100

    fix typo

--- configuration.nix
+++ configuration.nix
@@ -199,7 +199,7 @@
     status-git="for dir in `find . -maxdepth 1 -type d`; do cd $dir; pwd; git st; cd - >/dev/null; done";
     #stm32flasher="_stm32flash() { /usr/local/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32_Programmer_CLI --connect port=SWD freq=12000 --write $1 0x08000000 -g;}; _stm32flash";
     #stm32reset="/usr/local/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32_Programmer_CLI --connect port=SWD freq=12000 -hardRst";
-   }
+   };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
