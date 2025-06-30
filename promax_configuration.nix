ad07b976d4fc92173a2e8091eb71b82a14e7cd6d
Fabien RIBES <fabien.ribes@eove.fr>
2025-06-30 09:59:56 +0200

    exclude unused gnome packages

--- promax_configuration.nix
+++ promax_configuration.nix
@@ -169,6 +169,24 @@ in
     zoxide
   ];
 
+  environment.gnome.excludePackages = (with pkgs; [
+    atomix # puzzle game
+    cheese # webcam tool
+    epiphany # web browser
+    evince # document viewer
+    geary # email reader
+    gedit # text editor
+    gnome-characters
+    gnome-music
+    gnome-photos
+    gnome-terminal
+    gnome-tour
+    hitori # sudoku game
+    iagno # go game
+    tali # poker game
+    totem # video player
+  ]);
+
   virtualisation.docker.enable = true;
 
   fonts.packages = [
